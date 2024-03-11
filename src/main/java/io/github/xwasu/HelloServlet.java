0911d5e663f826101e7152da898dd16266fcd5db
xwasu <paatryk.wasik@gmail.com>
2024-03-11 20:55:14 +0100

    Request parameters

--- src/main/java/io/github/xwasu/HelloServlet.java
+++ src/main/java/io/github/xwasu/HelloServlet.java
@@ -9,14 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(name = "Hello", urlPatterns = {"/api/*"})
 public class HelloServlet extends HttpServlet {
+    private static final String NAME_PARAM = "name";
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("Request got");
-        resp.getWriter().write("Hello world!");
+        logger.info("Got request with parameters " + req.getParameterMap());
+        var name = Optional.ofNullable(req.getParameter(NAME_PARAM)).orElse("world");
+        resp.getWriter().write("Hello " + name + "!");
     }
 }
