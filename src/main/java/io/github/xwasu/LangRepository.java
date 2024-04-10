6969a3576fb101a83f378cd47b776edc846e0a38
xwasu <paatryk.wasik@gmail.com>
2024-04-10 13:12:44 +0200

    Using Hibernate in the repository

--- src/main/java/io/github/xwasu/LangRepository.java
+++ src/main/java/io/github/xwasu/LangRepository.java
@@ -5,17 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 class LangRepository {
-    private List<Lang> languages;
-
-    LangRepository() {
-        languages = new ArrayList<>();
-        languages.add(new Lang(1, "Hello", "en"));
-        languages.add(new Lang(2, "Siemanko", "pl"));
-    }
-
     Optional<Lang> findById(Integer id) {
-        return languages.stream()
-                .filter(l -> l.getId().equals(id))
-                .findFirst();
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        var result = session.get(Lang.class, id);
+        transaction.commit();
+        session.close();
+        return Optional.ofNullable(result);
     }
 }
