e41a01bb812c9ba0f5b84d0b1c75bb966bace405
xwasu <paatryk.wasik@gmail.com>
2024-04-24 22:01:38 +0200

    Repository: findAll

--- src/main/java/io/github/xwasu/LangRepository.java
+++ src/main/java/io/github/xwasu/LangRepository.java
@@ -1,14 +1,28 @@
 package io.github.xwasu;
 
+import java.util.List;
 import java.util.Optional;
 
 class LangRepository {
+    List<Lang> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.createQuery("from Lang ", Lang.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+
     Optional<Lang> findById(Integer id) {
         var session = HibernateUtil.getSessionFactory().openSession();
         var transaction = session.beginTransaction();
-        var result = session.get(Lang.class, id);
+
+        var result = Optional.ofNullable(session.get(Lang.class, id));
+
         transaction.commit();
         session.close();
-        return Optional.ofNullable(result);
+        return result;
     }
 }
