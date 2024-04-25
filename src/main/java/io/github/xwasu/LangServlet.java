78aa0ca1935b57dd295ef9d75d07c4fc98a9b0c4
xwasu <paatryk.wasik@gmail.com>
2024-04-25 19:13:06 +0200

    Returning JSON from Servlet

---
+++ src/main/java/io/github/xwasu/LangServlet.java
@@ -0,0 +1,40 @@
+package io.github.xwasu;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@WebServlet(name = "Lang", urlPatterns = {"/api/langs"})
+public class LangServlet extends HttpServlet {
+    private final Logger logger = LoggerFactory.getLogger(LangServlet.class);
+
+    private LangRepository repository;
+    private ObjectMapper mapper;
+
+    /**
+     * Servlet container needs it.
+     */
+    @SuppressWarnings("unused")
+    public LangServlet() {
+        this(new LangRepository(), new ObjectMapper());
+    }
+
+    LangServlet(LangRepository repository, ObjectMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        logger.info("Got request with parameters: " + req.getParameterMap());
+        resp.setContentType("application/json;charset=UTF-8");
+        mapper.writeValue(resp.getOutputStream(), repository.findAll());
+    }
+}
